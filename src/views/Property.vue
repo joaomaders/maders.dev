55457962249f1cea40101417d3a03b988bbf2e3c
João Mathias <joaomadersz@gmail.com>
2023-02-19 16:34:57 -0300

    adjust property page

--- src/views/Property.vue
+++ src/views/Property.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
-    <h1>This website is owned and operated by Joao Maders</h1>
-
+    <h2>Este site pertence e é operado por Joao Maders</h2>
     <p> E-mail: joaomadersz@gmail.com </p>
-    <p> Telefone: +55 (51) 9 9548-9285 </p>
+    <p> Telefone: +55 51995489285 </p>
+
+    <h2>This website is owned and operated by Joao Maders</h2>
+    <p> E-mail: joaomadersz@gmail.com </p>
+    <p> Phone: +55 51995489285 </p>
   </div>
 </template>
 
